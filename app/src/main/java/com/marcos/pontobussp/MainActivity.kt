ebeb80ca17a3b759fa3936a9a21e8cc5b683b548
Marcos Sales <m4rcos.sales@gmail.com>
2024-07-04 18:27:31 -0300

    feat: finalizando layout

--- app/src/main/java/com/marcos/pontobussp/MainActivity.kt
+++ app/src/main/java/com/marcos/pontobussp/MainActivity.kt
@@ -10,16 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
-        return true
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 }
