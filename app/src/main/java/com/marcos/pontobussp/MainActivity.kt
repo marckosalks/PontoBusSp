3faf3ccaadacebd2df106a40000225cd91324255
Marcos Sales <m4rcos.sales@gmail.com>
2024-07-10 02:25:38 -0300

    feat: add search view

--- app/src/main/java/com/marcos/pontobussp/MainActivity.kt
+++ app/src/main/java/com/marcos/pontobussp/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.marcos.pontobussp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.search.SearchView
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
